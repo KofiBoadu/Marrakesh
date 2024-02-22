5987670814bc0b54514e767043d02a0cb335d28a
Phyllis Kodua <pkodua@users-iMac.attlocal.net>
2024-02-21 22:22:09 -0600

    styling profile edit buttons and forms

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -49,3 +49,131 @@ function openEmailForm() {
       emailSendBody.style.display = 'block';
     }
   }
+
+  // function toggleUpdateNameForm() {
+  //   var formContainer = document.getElementById('formContainer');
+  //   formContainer.classList.toggle('hidden');
+  // }
+  
+ 
+  // document.getElementById('edit').addEventListener('click', function() {
+  //   toggleFormVisibility();
+  // });
+
+
+  function showUpdateNameForm(){
+    var nameForm =document.getElementById('formContainer')
+    var editButton = document.querySelector(".edit")
+    nameForm.style.display="block"
+    editButton.style.display="block"
+
+  }
+  function closeUpdateNameForm() {
+    document.getElementById('formContainer').style.display = 'none';
+    var nameForm = document.getElementById('formContainer').querySelector('.update_customer_name');
+    if (nameForm) {
+        nameForm.reset();
+    }
+    var editButton = document.querySelector(".edit")
+    editButton.style.display="none"
+  }
+  function editButtonDisplay(){
+    document.querySelector('.edit').style.display='none'
+  }
+
+  function showEditButton(){
+    let editButton = document.querySelector(".edit")
+    editButton.style.display="inline-block"
+  }
+  function hideEditButton(event) {
+    let editButton = document.querySelector(".edit");
+    var nameForm = document.getElementById('formContainer').querySelector('.update_customer_name');
+
+    // Check if the mouseout occurred outside the nameForm and outside the editButton
+    if (nameForm.contains(event.relatedTarget) ) {
+        // Hide the edit button
+        editButton.style.display = "block";
+    }
+    if(event.relatedTarget !== editButton){
+      editButton.style.display = "none";
+    }
+  }
+
+//phone edit
+function showUpdatePhoneForm(){
+  var phoneForm =document.getElementById('phoneformContainer')
+  var phoneEditButton = document.querySelector(".editPhone")
+  phoneForm.style.display="block"
+  phoneEditButton.style.display="block"
+
+}
+function closeUpdatePhoneForm() {
+  document.getElementById('phoneformContainer').style.display = 'none';
+  var phoneForm = document.getElementById('phoneformContainer').querySelector('.update_customer_phone');
+  if (phoneForm) {
+      phoneForm.reset();
+  }
+  var phoneEditButton = document.querySelector(".phoneEdit")
+  phoneEditButton.style.display="none"
+}
+function phoneEditButtonDisplay(){
+  document.querySelector('.phoneEdit').style.display='none'
+}
+
+function showPhoneEditButton(){
+  let phoneEditButton = document.querySelector(".phoneEdit")
+  phoneEditButton.style.display="inline-block"
+}
+function phoneHideEditButton(event) {
+  let phoneEditButton = document.querySelector(".phoneEdit");
+  var phoneForm = document.getElementById('phoneformContainer').querySelector('.update_customer_phone');
+
+  // Check if the mouseout occurred outside the nameForm and outside the editButton
+  if (phoneForm.contains(event.relatedTarget) ) {
+      // Hide the edit button
+      phoneEditButton.style.display = "block";
+  }
+  if(event.relatedTarget !== phoneEditButton){
+    phoneEditButton.style.display = "none";
+  }
+}
+
+//email edit
+function showUpdateEmailForm(){
+  var emailForm =document.getElementById('emailformContainer')
+  var emailEditButton = document.querySelector(".emailEdit")
+  emailForm.style.display="block"
+  emailEditButton.style.display="block"
+
+}
+function closeUpdateEmailForm() {
+  document.getElementById('emailformContainer').style.display = 'none';
+  var emailForm =document.getElementById('emailformContainer').querySelector('.update_customer_email');
+  if (emailForm ) {
+    emailForm .reset();
+  }
+  var emailEditButton = document.querySelector(".emailEdit")
+  emailEditButton.style.display="none"
+}
+function emailEditButtonDisplay(){
+  document.querySelector('.emailEdit').style.display='none'
+}
+
+function showEmailEditButton(){
+  let emailEditButton = document.querySelector(".emailEdit")
+  emailEditButton.style.display="inline-block"
+}
+function emailHideEditButton(event) {
+  let emailEditButton = document.querySelector(".emailEdit")
+  var emailForm = document.getElementById('emailformContainer').querySelector('.update_customer_email');
+
+  // Check if the mouseout occurred outside the nameForm and outside the editButton
+  if (emailForm.contains(event.relatedTarget) ) {
+      // Hide the edit button
+      emailEditButton.style.display = "block";
+  }
+  if(event.relatedTarget !== emailEditButton){
+    emailEditButton.style.display = "none";
+  }
+}
+
