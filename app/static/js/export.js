ec53c6c9cfbb6b84631c6485799cbe7c5946be2e
Phyllis Kodua <pkodua@users-iMac.attlocal.net>
2024-02-27 01:59:38 -0600

    styling export

--- app/static/js/export.js
+++ app/static/js/export.js
@@ -8,3 +8,7 @@ function show_exportButton(){
 	document.getElementById('export_form').style.display='block';
 	
 }
+
+function closeExportModal(){
+	document.getElementById('export_form').style.display='None';
+}
