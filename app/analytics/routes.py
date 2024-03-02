e6b03c5f815b90a8463473952058426c627cd4e6
Daniel Kofi Boadu <mrboadu3@gmail.com>
2024-03-01 21:37:32 -0600

    analytics chart

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -2,7 +2,8 @@ from flask import  render_template, request,redirect,url_for,flash
 from . import analytics
 from app.models import get_total_numberOfTravellers,calculate_gross_revenue
 import datetime
-
+from app.reports import customers_location_by_state
+from flask import Flask, jsonify
 
 
 
@@ -15,9 +16,14 @@ def analytics_home():
     revenue, formatted_revenue = calculate_gross_revenue(year)
     return render_template("analytics.html", total_travelers=total_travelers, year=year, revenue=revenue, formatted_revenue=formatted_revenue)
 
-# @analytics.route('/analytics',methods=['GET'])
-# def analytics_home():
-# 	year= datetime.datetime.now().year
-# 	total_travelers= get_total_numberOfTravellers()
-#     revenue,formatted_revenue= calculate_gross_revenue(year)
-# 	return render_template ("analytics.html",total_travelers=total_travelers,year=year,revenue=revenue)
+
+
+
+@analytics.route('/location_chart')
+def customers_location_charts():
+    data= customers_location_by_state()
+     # Convert the result to a list of dicts to jsonify it
+    data_list = [{'state_group': state, 'customer_count': count} for state, count in data]
+    return jsonify(data_list)
+
+
