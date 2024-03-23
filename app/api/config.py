9409337699cc458542f41844b1eadf8ab160aa97
Daniel Boadu <mrboadu3@gmail.com>
2024-03-23 13:50:44 -0500

    facebook leads webhook

--- app/api/config.py
+++ app/api/config.py
@@ -3,8 +3,22 @@ from app.user import get_user
 from werkzeug.security import generate_password_hash,check_password_hash
 from  app.models import create_database_connection
 import secrets
+import re
 
 auth = HTTPBasicAuth()
+
+
+patterns = {
+    "non_english": re.compile(r'[^\x00-\x7F]+'),  # Matches any non-ASCII character, common in non-English text.
+    "large_numbers": re.compile(r'\b\d{5,}\b'),  # Matches any number with five or more digits.
+}
+
+
+
+
+
+
+
 
 
 def generate_verify_token():
@@ -125,11 +139,10 @@ def normalize_key(key):
 
 
 
-def create_standardized_model(raw_data):
+def standardized_model_wordpress(raw_data):
     normalized_data = {}
-    for key, value in raw_data.items():
+    for key,value in raw_data.items():
         normalized_data[normalize_key(key)]=value
-
     required_contact_details= {'first_name', 'last_name', 'email', 'phone_number', 'gender', 'state'}
     standardized_model = {
         "first_name": None,
@@ -138,17 +151,79 @@ def create_standardized_model(raw_data):
         "phone_number": None,
         "gender": None,
         "state": None,
-        "source":"wordpress",
         "form_data":{}
     }
-
     for key, value in normalized_data.items():
         if key in required_contact_details:
             standardized_model[key] = value
         else:
             standardized_model["form_data"][key] = value
-
     return standardized_model
 
+
+
+
+
+
+
+
+def standardized_model_facebook(raw_data):
+    standardized_model = {
+        'first_name': None,
+        'last_name': None,
+        'email': None,
+        'phone_number': None,
+        'gender': None,
+        'state': None,
+        'form_data': {}
+    }
+    normalized_data = {normalize_key(key): value for key, value in raw_data.items()}
+    for key, value in normalized_data.items():
+        if key in standardized_model:
+            standardized_model[key] = value
+        else:
+            standardized_model['form_data'][key] = value
+    return standardized_model
+
+
+
+
+def is_spam(request_data):
+
+    for field in ['first_name', 'last_name', 'message']:
+        if patterns['non_english'].search(request_data.get(field, '')):
+            return True
+
+    for field in ['number_of_days', 'number_of_people', 'budget']:
+        value = request_data.get(field, '0').replace(' ', '')
+        if patterns['large_numbers'].search(value):
+            return True
+
+    from_date = request_data.get('from_date', '')
+    to_date = request_data.get('to_date', '')
+    if from_date > to_date:
+        return True
+
+    return False
+
+
+
+
+
+
+
+
+
+# # Sample raw data passed to the function
+# raw_data =  {'Are you prepared to secure your spot with a deposit within the next week?': 'No', 'Are you ready to join one of our exclusive, limited-spot group trips in the next 3 months?': 'Maybe, Later', 'City': 'San Francisco', 'Create date': '2024-03-23T01:52:36+0000', 'Email': 'georgejurand03@gmail.com', 'First name': 'George', 'How many spots would you like to reserve for our upcoming group trip? (Please specify the number of travelers)': '2', 'Last name': 'Jurand', 'Phone number': '+14155161635', "Select the African destination you're most interested in from our upcoming itineraries": 'GHANA October 15th-24th 2024', 'What will be the best time to reach you?': '2024-03-27T02:00:00+0000'}
+# # Normalize and update the structure of the required_contact_details to match the normalized keys
+# # required_contact_details = {normalize_key(k) for k in {
+# #     'first_name', 'last_name', 'email', 'phone_number', 'gender', 'state'
+# # }}
+
+# # Adjust the function call if necessary
+
+# standardized_data = standardized_model_facebook(raw_data)
+# print(standardized_data)
 
 
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify,abort
 from . import api_blueprint
-from  .config import auth,create_leads,create_standardized_model,create_submissions,add_submission_data
+from  .config import auth,create_leads,standardized_model_wordpress,create_submissions,add_submission_data,standardized_model_facebook,is_spam
 import logging
 from flask import current_app as app
 from dotenv import load_dotenv
@@ -16,7 +16,20 @@ VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 APP_SECRET = os.environ.get('APP_SECRET')
 SECRET_TOKEN = os.getenv('SECRET_TOKEN')
 
-
+spammy_patterns = {
+    "number_of_days": "8444",
+    "number_of_people": "1779",
+    "from_date": "1987-01-04",
+    "to_date": "1981-07-06",
+    "budget": "59301",
+    "accommodation_type": "Standard Hotels",
+    "destination": "Egypt",
+    "first_name": "Williamciz",
+    "last_name": "WilliamcizXP",
+    "email": "qsudfkjmzfqnejvj@onet.pl",
+    "phone_number": "88926932618",
+    "message": "узнать больше Футбольный аналитик"
+}
 
 
 
@@ -24,6 +37,10 @@ SECRET_TOKEN = os.getenv('SECRET_TOKEN')
 
 @api_blueprint.route('/adding-new-lead-3e7a8f9d-94593', methods=['POST'])
 def add_new_lead():
+    request_data = request.form
+    if is_spam(request_data):
+        abort(400, description="Spam detected")
+
     token = request.args.get('token')
     if token != SECRET_TOKEN:
         abort(403)
@@ -32,7 +49,7 @@ def add_new_lead():
         data = request.form.to_dict()
     else:
         data = request.json
-    contact=create_standardized_model(data)
+    contact=standardized_model_wordpress(data)
 
     if contact:
         contact_id = create_leads(
@@ -44,7 +61,7 @@ def add_new_lead():
             state=contact.get('state')
         )
         if contact_id:
-            source=contact.get('source')
+            source="wordpress"
             submission_id=create_submissions(contact_id,source)
 
             if submission_id:
@@ -61,8 +78,32 @@ def add_new_lead():
 @api_blueprint.route('/adding-new-lead-3e7a8f9d-94593/facebook-ads-leads-gen', methods=['POST'])
 @auth.login_required
 def face_book_leads():
-    lead_data= request.json
-    print(lead_data)
-    return jsonify({'success': True}),200
+    if request.content_type == 'application/x-www-form-urlencoded':
+        data = request.form.to_dict()
+    else:
+        data = request.json
+    contact=standardized_model_facebook(data)
+    if contact:
+        contact_id = create_leads(
+            first_name=contact.get('first_name'),
+            last_name=contact.get('last_name'),
+            email=contact.get('email'),
+            phone=contact.get('phone_number'),
+            gender=contact.get('gender'),
+            state=contact.get('state')
+        )
+        if contact_id:
+            source="facebook"
+            submission_id=create_submissions(contact_id,source)
+            if submission_id:
+                for key,value in contact['form_data'].items():
+                    add_submission_data(submission_id,key,value)
+        return jsonify({"contact_id": contact_id}),200
+
+    else:
+        return jsonify({"error": "Invalid contact data"}),400
+
+
+
 
 
